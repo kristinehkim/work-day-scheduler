cd17311f1b9c7203e5a0671bb6fc458f0c9dd694
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-05-02 10:04:28 -0700

    properly saving in localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,33 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
 $(document).ready(function () {
 
   let currentDay = dayjs().format('dddd, MMMM D');
   $('#currentDay').text(currentDay);
-  // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-  // the code isn't run until the browser has finished rendering all the elements
-  // in the html.
-  $('#9').children('textarea').val(localStorage.getItem('9'))
-  $('#10').children('textarea').val(localStorage.getItem('10'))
-  $('#11').children('textarea').val(localStorage.getItem('11'))
-
+  // $('#9').children('textarea').val(localStorage.getItem('9'))
+  // $('#10').children('textarea').val(localStorage.getItem('10'))
+  // $('#11').children('textarea').val(localStorage.getItem('11'))
+  // iterating through each element of the time-block
+  $('.time-block').each(function (element) {
+    let id = $(this).attr('id');//this is refering to the time-block element and grabbing the id attribute
+    let storedEntry = localStorage.getItem(id);//getting the id from localStorage and saving it as a variable
+    $(this).children('textarea').val(storedEntry);//grabbing the textarea value(key) and storedEntry(value) from localStorage from each time-block so everything can stay on the page when it refreshes
+  })
+//get the hour dayjs if data-hour is < current hour addClass past
+  function getScheduleTime() {
+if (dayjs().format('h:mm A') < data-hour)
+  }
   
+  // let timeBlockPast = $('.time-block');
+  // timeBlockPast.addClass('past');
+  // timeBlockPast.css('background-color', '#d3d3d3');
+  // let timeBlockPresent = $('.time-block');
+  // timeBlockPresent.addClass('present');
+  // timeBlockPresent.css('background-color', '#ff6961');
+  // let timeBlockFuture = $('.time-block');
+  // timeBlockFuture.addClass('future');
+  // timeBlockFuture.css('background-color', '#77dd77');
 
 
   // TODO: Add a listener for click events on the save button. This code should
@@ -31,10 +49,10 @@ $(document).ready(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 
-  $('.saveBtn').on('click', function () {
-    let textValue = $(this).siblings('textarea').val()
-    let time = $(this).parent().attr('id')
-    localStorage.setItem(time, textValue)
+  $('.saveBtn').on('click', function () {//event listener on the click of the saveBtn, the function passes the block below
+    let textValue = $(this).siblings('textarea').val();//this is referring to saveBtn, textarea is the sibling, and getting the value of textarea
+    let time = $(this).parent().attr('id');//grabbing the id of the parent of saveBtn
+    localStorage.setItem(time, textValue);//setting the time which is the key and textValue which is the value in localStorage
   })
 
 });
