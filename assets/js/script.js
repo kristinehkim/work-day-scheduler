7600c11317b7fd40eb2e8368b21289768752b948
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-05-02 19:45:03 -0700

    past, present, future colors applied

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,50 +6,29 @@ let currentDay = dayjs().format('dddd, MMMM D');
 $(document).ready(function () {
 
   $('#currentDay').text(currentDay);
-  // $('#9').children('textarea').val(localStorage.getItem('9'))
+  // Below finds the div id=9, goes to the child textarea, gets the value (what the user types) from the key (9, 10, etc.) in localStorage
+  // $('#9').children('textarea').val(localStorage.getItem('9')) when you getItem, put the key and returns the value
   // $('#10').children('textarea').val(localStorage.getItem('10'))
   // $('#11').children('textarea').val(localStorage.getItem('11'))
-  // iterating through each element of the time-block
+  // iterating through each element of the time-block instead of doing the above over and over again until 17 (DRY Don't Repeat Yourself)
   $('.time-block').each(function (element) {
     let id = $(this).attr('id');//this is refering to the time-block element and grabbing the id attribute
     let storedEntry = localStorage.getItem(id);//getting the id from localStorage and saving it as a variable
     $(this).children('textarea').val(storedEntry);//grabbing the textarea value(key) and storedEntry(value) from localStorage from each time-block so everything can stay on the page when it refreshes
   })
-
+  setInterval(function () {
   $('.time-block').each(function (element) {
     let id = $(this).attr('id');
-    let currentTime = dayjs().format('h:mm A');
-    let timeBlockPresent = $('.time-block');
-    let timeBlockFuture = $('.time-block');
-    let timeBlockPast = $('.time-block');
+    let currentTime = dayjs().hour();
     if (currentTime == id) {
-      timeBlockPresent.addClass('present');
-      timeBlockPresent.css('background-color', '#ff6961');
+      $(this).addClass('present');
     } else if (currentTime < id) {
-      timeBlockFuture.addClass('future');
-      timeBlockFuture.css('background-color', '#77dd77');
+      $(this).addClass('future');
     } else (currentTime > id) 
-      timeBlockPast.addClass('past');
-      timeBlockPast.css('background-color', '#d3d3d3');
-  })
-  //get the hour dayjs if data-hour is < current hour addClass past
-  //   function getScheduleTime(element) {
-  // if (dayjs().format('h:mm A') < blockTime) {
-  //   let timeBlockFuture = $('.time-block');
-  //   timeBlockFuture.addClass('future');
-  //   timeBlockFuture.css('background-color', '#77dd77');
-  // }
-  //   } 
-
-  // let timeBlockPast = $('.time-block');
-  // timeBlockPast.addClass('past');
-  // timeBlockPast.css('background-color', '#d3d3d3');
-  // let timeBlockPresent = $('.time-block');
-  // timeBlockPresent.addClass('present');
-  // timeBlockPresent.css('background-color', '#ff6961');
-
-
-
+    $(this).addClass('past');
+  // console.log("hello")
+  });
+}, 120000)
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -70,10 +49,9 @@ $(document).ready(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 
-  $('.saveBtn').on('click', function () {//event listener on the click of the saveBtn, the function passes the block below
-    let textValue = $(this).siblings('textarea').val();//this is referring to saveBtn, textarea is the sibling, and getting the value of textarea
-    let time = $(this).parent().attr('id');//grabbing the id of the parent of saveBtn
-    localStorage.setItem(time, textValue);//setting the time which is the key and textValue which is the value in localStorage
+  $('.saveBtn').on('click', function () {//When we click(action) on the saveBtn, we want to save two values
+    let textValue = $(this).siblings('textarea').val();//this is referring to saveBtn of the corresponding textarea (they are siblings), and getting the value of textarea
+    let time = $(this).parent().attr('id');//Went to the saveBtn's parent (div) to grab the id
+    localStorage.setItem(time, textValue);//setting the the two variables, time which is the key and textValue which is the value in localStorage
   })
-
 });
